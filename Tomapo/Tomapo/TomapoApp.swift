e69607b91c833a1c3ad621b5c1b363962ac6ccc0
Sophia Moos <sophia.moos@outlook.com>
2026-03-27 13:56:00 +0100

    added theme colors and created project

---
+++ Tomapo/Tomapo/TomapoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomapoApp.swift
+//  Tomapo
+//
+//  Created by Sophia Moos on 27.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct TomapoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tomapo/TomapoTests/TomapoTests.swift
@@ -0,0 +1,19 @@
+//
+//  TomapoTests.swift
+//  TomapoTests
+//
+//  Created by Sophia Moos on 27.03.2026.
+//
+
+import Testing
+@testable import Tomapo
+
+struct TomapoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
